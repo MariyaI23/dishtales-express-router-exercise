684d3650aab9bcf14293f537fb03e6ad7cb979bb
Mariya Ivanova <m80ivanova@gmail.com>
2021-04-12 11:18:50 -0500

    Express recipeRouter

---
+++ routes/recipeRouter.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const recipeRouter = express.Router();
+
+recipeRouter.route("/")
+.all((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();
+})
+.get((req, res) => {
+  res.end("Will send all the recipes to you");
+})
+.post((req, res) => {
+  res.end(
+    `Will add the recipe: ${req.body.name} with directions: ${req.body.directions}`
+  );
+})
+.put((req, res) => {
+  res.statusCode = 403;
+  res.end("PUT operation not supported on /recipes");
+})
+.delete((req, res) => {
+  res.end("Deleting all recipes");
+});
+
+
+recipeRouter.route("/:recipeId")
+.all((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  next();
+})
+.get((req, res) => {
+  res.end(`Will send details of the recipe: ${req.params.recipeId} to you`);
+})
+.post((req, res) => {
+  res.statusCode = 403;
+  res.end(`POST opration not supported on /recipes/${req.params.recipeId}`);
+})
+.put((req, res) => {
+  res.write(`Updating the recipe: ${req.params.recipeId}\n`);
+  res.end(
+    `Will update the recipe: ${req.body.name} with directions: ${req.body.directions}`
+  );
+})
+.delete((req, res) => {
+  res.end(`Deleting recipe: ${req.params.recipeId}`);
+});
+
+
+module.exports = recipeRouter;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const recipeRouter = require("./routes/recipeRouter");
 
 const hostname = "localhost";
 const port = 3001;
@@ -8,46 +9,8 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.all("/recipes", (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-});
+app.use("/recipes", recipeRouter);
 
-app.get("/recipes", (req, res) => {
-    res.end("Will send all the recipes to you");
-});
-
-app.post("/recipes", (req, res) => {
-    res.end(`Will add the recipe: ${req.body.name} with directions: ${req.body.directions}`);
-});
-
-app.put("/recipes", (req, res) => {
-    res.statusCode = 403;
-    res.end("PUT operation not supported on /recipes");
-});
-
-app.delete("/recipes", (req, res) => {
-    res.end("Deleting all recipes");
-});
-
-app.get("/recipes/:recipeId", (req, res) => {
-    res.end(`Will send details of the recipe: ${req.params.recipeId} to you`);
-});
-
-app.post("/recipes/:recipeId", (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST opration not supported on /recipes/${req.params.recipeId}`);
-});
-
-app.put("/recipes/:recipeId", (req, res) => {
-    res.write(`Updating the recipe: ${req.params.recipeId}\n`);
-    res.end(`Will update the recipe: ${req.body.name} with directions: ${req.body.directions}`);
-});
-
-app.delete("/recipes/:recipeId", (req, res) => {
-    res.end(`Deleting recipe: ${req.params.recipeId}`);
-});
 
 app.use(express.static(__dirname + "/public"));
 
