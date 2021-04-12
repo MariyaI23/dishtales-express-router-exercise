3df75f202efc27312988769a88ac62cb068d114b
Mariya Ivanova <m80ivanova@gmail.com>
2021-04-12 12:02:05 -0500

    Express breakfastsRouter

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const recipeRouter = require("./routes/recipeRouter");
+const breakfastsRouter = require("./routes/breakfastsRouter");
 
 const hostname = "localhost";
 const port = 3001;
@@ -10,6 +11,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/recipes", recipeRouter);
+app.use("/breakfasts", breakfastsRouter);
 
 
 app.use(express.static(__dirname + "/public"));
